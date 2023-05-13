3f8e0e749d103446d83c0ba6e1190a6820d35a76
dijksterhuis <general@dijksterhuis.co.uk>
2023-05-13 22:39:15 +0100

    [UPDATE] Remove previous map specific configs (we can migrate over later).

--- maps/vn_khe_sanh/map_config/init.hpp
+++ maps/vn_khe_sanh/map_config/init.hpp
@@ -1,14 +1,4 @@
 class map_config {
-	max_hqs_per_zone = 1;
-	max_factories_per_zone = 1;
-	max_camps_per_zone = 5;
-	max_aa_per_zone = 10;
-	max_artillery_per_zone = 3;
-	max_fortifications_per_zone = 0;
-	max_tunnels_per_zone = 3;
-	max_radars_per_zone = 2;
-	max_water_supply_per_zone = 2;
-	max_vehicle_depots_per_zone = 3;
 	starting_zones[] = {"zone_khe_sanh", "zone_kok", "zone_french_fort"};
 	class zones {
 		#include "zones.hpp"
--- maps/vn_the_bra/map_config/init.hpp
+++ maps/vn_the_bra/map_config/init.hpp
@@ -1,14 +1,4 @@
 class map_config {
-	max_hqs_per_zone = 1;
-	max_factories_per_zone = 1;
-	max_camps_per_zone = 5;
-	max_aa_per_zone = 5;
-	max_artillery_per_zone = 4;
-	max_fortifications_per_zone = 0;
-	max_tunnels_per_zone = 3;
-	max_radars_per_zone = 2;
-	max_water_supply_per_zone = 2;
-	max_vehicle_depots_per_zone = 3;
 	starting_zones[] = {"zone_nam_phat", "zone_ban_pakha", "zone_ban_dac_maruk"};
 	class zones {
 		#include "zones.hpp"
