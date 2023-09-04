d46f3559fdbad70112464159c584c2c4b3b28b20
dijksterhuis <general@dijksterhuis.co.uk>
2023-09-04 21:21:56 +0100

    [FIXUP] Reduce AA site count from 10 (20 possible ZPUs) to 7 (14 possible ZPUs).

--- maps/cam_lao_nam/map_config/init.hpp
+++ maps/cam_lao_nam/map_config/init.hpp
@@ -1,7 +1,7 @@
 class map_config {
 	starting_zones[] = {"zone_ba_ria", "zone_ban_hoang"};
 	max_camps_per_zone = 3;
-	max_aa_per_zone = 10;
+	max_aa_per_zone = 7;
 	max_artillery_per_zone = 3;
 	max_fortifications_per_zone = 0;
 	max_radars_per_zone = 2;
--- maps/vn_khe_sanh/map_config/init.hpp
+++ maps/vn_khe_sanh/map_config/init.hpp
@@ -1,6 +1,6 @@
 class map_config {
 	max_camps_per_zone = 3;
-	max_aa_per_zone = 10;
+	max_aa_per_zone = 7;
 	max_artillery_per_zone = 3;
 	max_fortifications_per_zone = 0;
 	max_tunnels_per_zone = 3;
